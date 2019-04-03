f52436ecd46cc0c7f9c06789f941727d41df7c15
b.yordani93@gmail.com <b.yordani93@gmail.com>
2019-04-03 14:40:23 -0600

    init

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+//export our MongoDB URI(Uniform Resource Identifier)
+module.exports = {
+    mongoURI: "mongodb+srv://XeX:GWNAsVyolar6Z50N@mvp-mnzso.mongodb.net/test?retryWrites=true"
+};
---
+++ server/server.js
@@ -0,0 +1,30 @@
+//find and use technologies we will use.
+const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+//bodyparse middleware
+app.use(bodyParser.urlencoded({
+    extended: false
+})); //parse the url
+
+app.use(bodyParser.json());//parse to jason
+
+const db = require("/home/hc-19/Carlosf96/MVP/config/keys.js").mongoURI;//require config for DB
+
+mongoose
+    .connect(
+        db, {
+            useNewUrlParser: true
+        })
+    .catch(err => console.log(err))
+    .then(() => console.log("MongoDB connected successfully!"))
+
+//let server listen on defined port
+const port = 1337
+//const port = process.env.PORT || 5000; process.env.port is Heroku's port if you choose to deploy the app there
+
+app.listen(port, () => {
+    console.log(`App listening on ${port}!`);
+});

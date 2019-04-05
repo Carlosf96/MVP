e9012d126e4aa904be2ff4619c81540f2a69aa86
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-05 09:45:34 -0600

    cleaned up cra and started working on navbar component

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className='navbar-fixed' 
+      </div>
+    )
+  }
+}

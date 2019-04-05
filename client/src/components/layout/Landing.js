fa7cf921024e9059979cc019c64b774835d4cff5
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-05 10:09:04 -0600

    Implementing landing component

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ height: '75vh'}} className='container valign-wrapper'>
+        <div className='row'>
+          <div className='col s12 center-align'>
+            <h4>
+              <b>Build</b> a login/auth app with the{" "}
+              <span style={{ fontFamily: 'monospace' }}>MERN</span>
+              stack from scratch
+            </h4>
+            <p className='flow-text grey-text text-darken-1'>
+              create a (minimal) full-stack app with user auth via passportJS and JWT
+            </p>
+            <br/>
+            <a
+              style={{
+                width: '150px',
+                borderRadius: '3px',
+                letterSpacing: '1.5px'
+              }}
+              className='btn btn-large waves-effect waves-liht hoverable blue accent-3'>
+              Register
+            </a>
+            <a
+              style={{
+                marginLeft: '2rem',
+                width: '150px',
+                borderRadius: '3px',
+                letterSpacing: '1.5px'
+              }}
+            >
+
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Landing;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,8 +6,22 @@ export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className='navbar-fixed' 
+        <nav className='navbar-fixed'>
+          <div className='nav-wrapper white'>
+            <Link 
+              to='/'
+              style={{
+                fontFamily: 'monospace'
+              }}
+              className='col s5 brand-logo cener black-text'>
+              <i className='material-icons'>code</i>{/*airbnb style guide says always self close tags that dont have children*/}
+              </Link>
+              FETUS
+          </div>
+        </nav> 
       </div>
     )
   }
 }
+
+export default Navbar;

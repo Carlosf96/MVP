7a19c13743ab2d427fcbe8b8193fa40591d304d4
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-11 13:59:51 -0500

    Modified path to user api again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const users = require('routes/api/users.js');
+const users = require('./routes/api/users');
 const app = express();
 const path = require('path');
 //bodyparse middleware

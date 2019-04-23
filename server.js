e26502cfff75dd59c4ca861d3a2469726497113d
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-22 22:52:01 -0500

    Fix attempt

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use('/api/users', users)
 //server static assets if in production 
 if (process.env.NODE_ENV === 'production') {
    //set a static folder
-  //app.use(express.static('client/build')); 
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build')); 
+  //app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));

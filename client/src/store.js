b1318d263a7444bd92e79802f7960b08b76b6567
Carlosf96 <carlos.f.roque96@gmail.com>
2019-04-23 14:05:36 -0500

    Changed from composeWithDevtools to compose from redux

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const initialState={};
@@ -11,9 +11,11 @@ const middleware = [thunk];
 const store = createStore( 
   rootReducer,
   initialState,
-  composeWithDevTools(
-    applyMiddleware(...middleware)
-  )
+  compose(
+    applyMiddleware(...middleware),	    applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && //allows to use redux dev extension in browser	  window.__REDUX_DEVTOOLS_EXTENSION__ && //allows to use redux dev extension in browser
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+  )	
 );
 
 export default store;
